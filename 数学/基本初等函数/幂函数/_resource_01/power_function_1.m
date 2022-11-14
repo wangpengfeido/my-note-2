f524acc42561e30aedc11d9acf3091a62658aa8a
wangpengfeido <wangpengfeido@126.com>
2022-11-15 00:41:10 +0800

    数学

--- 数学/基本初等函数/幂函数/_resource_01/power_function_1.m
+++ 数学/基本初等函数/幂函数/_resource_01/power_function_1.m
@@ -1,7 +1,11 @@
-x = linspace(-2, 2);
-g = x.^2;
-k = x.^3;
+x1 = linspace(-2, 2);
+g = x1.^2;
+k = x1.^3;
 
-plot(x, g, x, k);
-legend('f(x)=x^{2}', 'f(x)=x^{3}')
+x2 = linspace(0, 2);
+l = x2.^(1/2);
+m = x2.^(1/3);
+
+plot(x1, g, 'r', x1, k, 'g', x2, l, 'b', x2, m, 'c');
+legend('$f(x)=x^{2}$', '$f(x)=x^{3}$', '$f(x)=x^{\frac{1}{2}}$', '$f(x)=x^{\frac{1}{3}}$', 'Interpreter', 'latex')
 grid on;
--- 数学/基本初等函数/幂函数/_resource_01/power_function_2.m
+++
@@ -1,7 +0,0 @@
-x = linspace(-10, 10);
-g = x.^(1/2);
-k = x.^(1/3);
-
-plot(x, g, x, k);
-legend({'$f(x)=x^{\frac{1}{2}}$', '$f(x)=x^{\frac{1}{3}}$'}, 'Interpreter', 'latex');
-grid on;
