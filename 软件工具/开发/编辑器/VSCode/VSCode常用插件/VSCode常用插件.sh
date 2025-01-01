68408e3a1e8cc5803746bc2350e215459fedb286
wangpengfeido <wangpengfeido@126.com>
2025-01-01 16:16:23 +0800

    菜谱

--- 软件工具/开发/编辑器/VSCode/VSCode常用插件/VSCode常用插件.sh
+++ 软件工具/开发/编辑器/VSCode/VSCode常用插件/VSCode常用插件.sh
@@ -1,14 +1,11 @@
-# VSCode 常用插件
-
-## 常用插件
-
-```shell
 # 注释更好地展示
 code --install-extension aaron-bond.better-comments
 # 颜色选择
 code --install-extension adam-watters.vscode-color-pick
-# matlab 代码格式化
-code --install-extension AffenWiesel.matlab-formatter
+# matlab 语言格式化支持
+code --install-extension affenwiesel.matlab-formatter
+# nginx 配置代码支持
+code --install-extension ahmadalli.vscode-nginx-conf
 # eclipse 热键
 code --install-extension alphabotsec.vscode-eclipse-keybindings
 # css 颜色展示
@@ -19,8 +16,8 @@ code --install-extension codezombiech.gitignore
 code --install-extension dbaeumer.vscode-eslint
 # git 增强。展示代码行 git 的提交身份、时间等
 code --install-extension eamodio.gitlens
-# editor config 代码格式化支持
-code --install-extension EditorConfig.EditorConfig
+# 支持 editConfig (https://editorconfig.org/)
+code --install-extension editorconfig.editorconfig
 # prettier 插件
 code --install-extension esbenp.prettier-vscode
 # flow.js 类型支持
@@ -30,19 +27,19 @@ code --install-extension formulahendry.auto-rename-tag
 # 运行代码
 code --install-extension formulahendry.code-runner
 # matlab 支持
-code --install-extension Gimly81.matlab
-# github 主题
-code --install-extension GitHub.github-vscode-theme
+code --install-extension gimly81.matlab
+# 主题：github 主题
+code --install-extension github.github-vscode-theme
 # go 语言支持
 code --install-extension golang.go
 # todo tree 展示
-code --install-extension Gruntfuggly.todo-tree
+code --install-extension gruntfuggly.todo-tree
 # yaml 与 json 互相转换
 code --install-extension hilleer.yaml-plus-json
 # AppleScript 支持
 code --install-extension idleberg.applescript
 # latex 支持
-code --install-extension James-Yu.latex-workshop
+code --install-extension james-yu.latex-workshop
 # 快速搜索 node_modules 文件
 code --install-extension jasonnutter.search-node-modules
 # svg 图片展示与编辑
@@ -55,20 +52,36 @@ code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension mhutchie.git-graph
 # .env 文件支持
 code --install-extension mikestead.dotenv
+# python 语言格式化
+code --install-extension ms-python.autopep8
+# python 语言 debug 支持
+code --install-extension ms-python.debugpy
+# python 语言对 import 进行排序
+code --install-extension ms-python.isort
 # python 语言支持
 code --install-extension ms-python.python
 # python 语言支持
 code --install-extension ms-python.vscode-pylance
+# jupyter 支持
+code --install-extension ms-toolsai.jupyter
+# jupyter 渲染器（支持图表、图片等输出）
+code --install-extension ms-toolsai.jupyter-renderers
+# jupyter 支持 cell tag
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+# jupyter 支持 slide
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
 # ssh 支持
 code --install-extension ms-vscode-remote.remote-ssh
 # ssh 配置文件
 code --install-extension ms-vscode-remote.remote-ssh-edit
 # C/C++ 语言支持
 code --install-extension ms-vscode.cpptools
-# vue 支持
-code --install-extension octref.vetur
+# ssh 文件浏览
+code --install-extension ms-vscode.remote-explorer
+# react native 支持
+code --install-extension msjsdiag.vscode-react-native
 # 静态资源服务
-code --install-extension ritwickdey.LiveServer
+code --install-extension ritwickdey.liveserver
 # 更多复制选项。可复制不带样式的文字。
 code --install-extension salbert.copy-text
 # markdown 预览增强
@@ -83,24 +96,11 @@ code --install-extension syler.sass-indented
 code --install-extension techer.open-in-browser
 # vscode 图标主题
 code --install-extension vscode-icons-team.vscode-icons
+# vue 支持
+code --install-extension vue.volar
 # todo 高亮
 code --install-extension wayou.vscode-todo-highlight
 # one dark pro 主题
 code --install-extension zhuangtongfa.material-theme
 # 跳转到 github
 code --install-extension ziyasal.vscode-open-in-github
-```
-
-## 如何导出插件列表
-
-unix:
-
-```
-code --list-extensions | xargs -L 1 echo code --install-extension
-```
-
-windows:
-
-```
-code --list-extensions | % { "code --install-extension $_" }
-```
---
+++ 软件工具/开发/编辑器/VSCode/VSCode常用设置/VSCode常用设置.md
@@ -0,0 +1,3 @@
+# VSCode 常用设置
+
+[VSCode 常用设置](./VSCode常用设置.jsonc)
